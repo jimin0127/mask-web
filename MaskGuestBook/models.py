26bc09d619c44d5c609ac884e5cd69db53947781
jimin0127 <ckdgh4662@naver.com>
2021-04-04 18:01:15 +0900

    GuestBook Model

--- MaskGuestBook/models.py
+++ MaskGuestBook/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class GuestBook(models.Model):
+    pic = models.ImageField(upload_to='MaskGuestBook/img/%y')
+    name = models.CharField(max_length=5)
+    phone = models.CharField(max_length = 15)
+    message = models.TextField()
+
+    def __str__(self):
+        return '{}, {}'.format(self.name, self.message)
