ffa2d6f5944b66a78eb1ad313c9a7d5216788843
jimin0127 <ckdgh4662@naver.com>
2021-04-04 23:34:17 +0900

    방명록 Form 작성, 저장, 화면 전환

--- MaskGuestBook/urls.py
+++ MaskGuestBook/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 from MaskGuestBook import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('index', views.index, name="index"),
+    path('GuestBook', views.GuestBook, name="GuestBook"),
 ]
--- MaskGuestBook/views.py
+++ MaskGuestBook/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import GuestBookForm
 
 # Create your views here.
+def GuestBook(request):
+    if request.method == 'POST':
+        form = GuestBookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return render(
+            #     request,
+            #     'MaskGuestBook/index.html',
+            #     {'form': form}
+            # )
+            return redirect('index')
+    else:
+        form = GuestBookForm()
+        return render(
+            request,
+            'MaskGuestBook/GuestBook.html',
+            {'form': form}
+        )
+
 def index(request):
+
     return render(
         request,
         'MaskGuestBook/index.html',
-        {}
+        {
+        }
     )
